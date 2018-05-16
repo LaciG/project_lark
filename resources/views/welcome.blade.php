48f293e8a347a2b25e2ea6eb32ef017df0165aca
László Gábor <lacigabi@windowslive.com>
2018-05-16 13:16:34 +0200

    Lang Fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,6 +78,20 @@
             @endif
 
             <div class="content">
+                    <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                {{ Config::get('languages')[App::getLocale()] }}
+                            </a>
+                            <ul class="dropdown-menu">
+                                @foreach (Config::get('languages') as $lang => $language)
+                                    @if ($lang != App::getLocale())
+                                        <li>
+                                            <a href="{{ route('lang.switch', $lang) }}">{{$language}}</a>
+                                        </li>
+                                    @endif
+                                @endforeach
+                            </ul>
+                        </li>
                 <div class="title m-b-md">
                     Laravel
                 </div>
